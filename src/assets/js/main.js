705d617d461d2f8ba4e165533bd6ec8ee7380a17
Robiul Hasan <robiulhasan9559@gmail.com>
2026-05-18 12:35:50 +0600

    (Section) blur shadow bottom added

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1149,4 +1149,23 @@
     }
   });
   // ========================= Header Sticky tw0 Js End===================
+
+  //  ==================== bottom blur shadow js start ====================
+  $(window).on("scroll", function () {
+    if (
+      $(window).scrollTop() + $(window).height() >=
+      $(document).height() - 5
+    ) {
+      $(".blur-bottom-shadow").css({
+        opacity: "0",
+        visibility: "hidden",
+      });
+    } else {
+      $(".blur-bottom-shadow").css({
+        opacity: "1",
+        visibility: "visible",
+      });
+    }
+  });
+  //  ==================== bottom blur shadow js end ====================
 })(jQuery);
